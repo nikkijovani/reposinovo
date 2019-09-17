5ee198aa3740180f5a4a42674fe509958c620454
nikkijovani <jovaniferreirainfo2017@gmail.com>
2019-09-17 18:17:31 -0300

    derrotado.com

---
+++ mavenproject1/mercado/src/main/java/br/edu/iff/mercado/servlet/loginclie.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.iff.mercado.servlet;
+
+import br.edu.iff.mercado.entidades.Cliente;
+import br.edu.iff.mercado.util.HibernateUtil;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author aluno
+ */
+public class loginclie extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet loginclie</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet loginclie at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+        
+        
+        String username = request.getParameter("NrCpf");
+        String senha = request.getParameter("DsSenha");
+        Session session= HibernateUtil.getSession();
+        Cliente user = (Cliente) session.createQuery("from usuario where username = ? and senha = ?")
+                .setString(0, username)
+                .setString(1, senha)
+                .uniqueResult();
+        if (user == null) {
+            response.sendRedirect("logininvalido.html");
+        } else {
+            response.sendRedirect("index.html");
+        }
+        
+
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
